d4be1f3d02e4984784152932d1fd8480a706ee31
lernrr777 <lernrr777@gmail.com>
2015-07-06 12:03:03 +0530

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -2,8 +2,9 @@
 // Here is how to define your module
 // has dependent on mobile-angular-ui
 //
-document.write('<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=weather,visualization,panoramio"></script>');
-document.write('<script src="https://rawgit.com/allenhwkim/angularjs-google-maps/master/build/scripts/ng-map.js"></script>');
+alert('current file');
+//document.write('<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=weather,visualization,panoramio"></script>');
+//document.write('<script src="https://rawgit.com/allenhwkim/angularjs-google-maps/master/build/scripts/ng-map.js"></script>');
 var app = angular.module('MobileAngularUiExamples', [
   'ngRoute',
   'ngAnimate',
